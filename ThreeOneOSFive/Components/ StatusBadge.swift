1e36273e30c7905ef8b911a350b07bb694211043
Shinn <shinndeptraiivcl@gmail.com>
2026-09-21 20:57:17 +0700

    Create  StatusBadge.swift

---
+++ ThreeOneOSFive/Components/ StatusBadge.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct StatusBadge: View {
+    let text: String
+    let color: Color
+
+    var body: some View {
+        Text(text)
+            .font(.caption2)
+            .padding(.horizontal, 6)
+            .padding(.vertical, 2)
+            .background(color.opacity(0.2))
+            .foregroundStyle(color)
+            .clipShape(Capsule())
+    }
+}
