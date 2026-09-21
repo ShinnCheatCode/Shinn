d44d3874ff79dce2af72a50c207e47ea27b1da51
Shinn <shinndeptraiivcl@gmail.com>
2026-09-21 22:41:30 +0700

    Update  Cleaner.swift

--- ThreeOneOSFive/Core/Cleaner/ Cleaner.swift
+++ ThreeOneOSFive/Core/Cleaner/ Cleaner.swift
@@ -1,8 +1,13 @@
 import Foundation
 
 final class Cleaner {
-    func scan(bundleID: String) -> CleanerScanResult { return CleanerScanResult() }
-    func delete(items: [CleanerItem]) -> CleanerResult { return CleanerResult() }
+    func scan(bundleID: String) -> CleanerScanResult {
+        CleanerScanResult(items: [], totalSize: 0)
+    }
+
+    func delete(items: [CleanerItem]) -> CleanerResult {
+        CleanerResult(deletedCount: 0, freedSize: 0)
+    }
 }
 
 struct CleanerScanResult {
