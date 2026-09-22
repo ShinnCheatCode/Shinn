302302f62e5f35424edd9b12e4d9f23e628417c3
Shinn <shinndeptraiivcl@gmail.com>
2026-09-22 12:18:10 +0700

    Update  ThreeOneOSFiveApp.swift

--- ThreeOneOSFive/App/ ThreeOneOSFiveApp.swift
+++ ThreeOneOSFive/App/ ThreeOneOSFiveApp.swift
@@ -1,3 +1,36 @@
+import SwiftUI
+
+@main
+struct ThreeOneOSFiveApp: App {
+    @StateObject private var appState = AppState()
+    @AppStorage("hasAcceptedTerms") private var hasAcceptedTerms: Bool = false
+    @AppStorage("selectedRole") private var selectedRoleRaw: String = ""
+    @State private var roleGranted: Bool = false
+
+    var body: some Scene {
+        WindowGroup {
+            Group {
+                if !hasAcceptedTerms {
+                    TermsView()
+                } else if !roleGranted && selectedRoleRaw.isEmpty {
+                    RoleGateView { role in
+                        selectedRoleRaw = role.rawValue
+                        roleGranted = true
+                    }
+                } else {
+                    RootView()
+                        .environmentObject(appState)
+                        .environmentObject(appState.shimCheat)
+                }
+            }
+            .onAppear {
+                if !selectedRoleRaw.isEmpty { roleGranted = true }
+                BackgroundMusic.shared.start()
+            }
+        }
+    }
+}
+
 struct TermsView: View {
     @AppStorage("hasAcceptedTerms") private var hasAcceptedTerms = false
 
